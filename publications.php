7cc0db222b89b06559468a7b4ffdfc1e2e19227a
theCrea8r <43229963+theCrea8r@users.noreply.github.com>
2020-02-13 14:58:05 +0530

    publications

--- publications.php
+++ publications.php
@@ -6,110 +6,98 @@
 				<div class="photograhy">
 					<div class="row no-gutters">
 						<div class="col-md-3">
-							<a href="#" class="photography-entry img d-flex justify-content-center align-items-center" style="background-image: url(images/image_1.jpg);">
+							<a href="#" class="photography-entry img d-flex justify-content-center align-items-center" style="background-image: url(cover/1.Buddha_Dharma_Darshan_Mein_Brahmavihāra_Bhāvanā.jpg);">
 								<div class="overlay"></div>
 								<div class="text text-center">
-									<h3>Work 01</h3>
-									<span>Technology</span>
+									<span>Buddha Dharma Darshan Mein Brahmavihāra Bhāvanā</span>
 								</div>
 							</a>
 						</div>
 						<div class="col-md-3">
-							<a href="#" class="photography-entry img d-flex justify-content-center align-items-center" style="background-image: url(images/image_2.jpg);">
+							<a href="#" class="photography-entry img d-flex justify-content-center align-items-center" style="background-image: url(cover/2.Jinālaṅkāra.jpg);">
 								<div class="overlay"></div>
 								<div class="text text-center">
-									<h3>Work 02</h3>
-									<span>Nature</span>
+									<span>Jinālaṅkāra</span>
 								</div>
 							</a>
 						</div>
 						<div class="col-md-3">
-							<a href="#" class="photography-entry img d-flex justify-content-center align-items-center" style="background-image: url(images/image_3.jpg);">
+							<a href="#" class="photography-entry img d-flex justify-content-center align-items-center" style="background-image: url(cover/3.Dāṭhāvaṃsa_History_of_Buddha’s_Teeth_Relic.jpg);">
 								<div class="overlay"></div>
 								<div class="text text-center">
-									<h3>Work 03</h3>
-									<span>Fashion</span>
+									<span>Dāṭhāvaṃsa History of Buddha’s Teeth Relic</span>
 								</div>
 							</a>
 						</div>
 						<div class="col-md-3">
-							<a href="#" class="photography-entry img d-flex justify-content-center align-items-center" style="background-image: url(images/image_4.jpg);">
+							<a href="#" class="photography-entry img d-flex justify-content-center align-items-center" style="background-image: url(cover/4.Anāgatavaṃsa_History_of_Mette_ya_Buddha.jpg);">
 								<div class="overlay"></div>
 								<div class="text text-center">
-									<h3>Work 04</h3>
-									<span>Travel</span>
+									<span>Anāgatavaṃsa History of Mette ya Buddha</span>
 								</div>
 							</a>
 						</div>
 						<div class="col-md-3">
-							<a href="#" class="photography-entry img d-flex justify-content-center align-items-center" style="background-image: url(images/image_5.jpg);">
+							<a href="#" class="photography-entry img d-flex justify-content-center align-items-center" style="background-image: url(cover/5.Chakesadhātuvaṃsa_History_of_Buddha’s_Six_Hair_Elements.jpg);">
 								<div class="overlay"></div>
 								<div class="text text-center">
-									<h3>Work 05</h3>
-									<span>Travel</span>
+									<span>Chakesadhātuvaṃsa History of Buddha’s Six Hair Elements</span>
 								</div>
 							</a>
 						</div>
 						<div class="col-md-3">
-							<a href="#" class="photography-entry img d-flex justify-content-center align-items-center" style="background-image: url(images/image_6.jpg);">
+							<a href="#" class="photography-entry img d-flex justify-content-center align-items-center" style="background-image: url(cover/6.Pañcagatidīpanī_Clarification_of_the_Five_Realms_of_Existence.jpg);">
 								<div class="overlay"></div>
 								<div class="text text-center">
-									<h3>Work 06</h3>
-									<span>Travel</span>
+									<span>Pañcagatidīpanī Clarification of the Five Realms of Existence</span>
 								</div>
 							</a>
 						</div>
 						<div class="col-md-3">
-							<a href="#" class="photography-entry img d-flex justify-content-center align-items-center" style="background-image: url(images/image_7.jpg);">
+							<a href="#" class="photography-entry img d-flex justify-content-center align-items-center" style="background-image: url(cover/7.Nāmarūpasamāsa_Summary_of_Mind_and_Matter.jpg);">
 								<div class="overlay"></div>
 								<div class="text text-center">
-									<h3>Work 07</h3>
-									<span>Fashion, Model</span>
+									<span>Nāmarūpasamāsa Summary of Mind and Matter</span>
 								</div>
 							</a>
 						</div>
 						<div class="col-md-3">
-							<a href="#" class="photography-entry img d-flex justify-content-center align-items-center" style="background-image: url(images/image_8.jpg);">
+							<a href="#" class="photography-entry img d-flex justify-content-center align-items-center" style="background-image: url(cover/8.Anagārika_Dhamapāla_The_Revialist_of_Buddhism.jpg);">
 								<div class="overlay"></div>
 								<div class="text text-center">
-									<h3>Work 08</h3>
-									<span>Nature</span>
+									<span>Anagārika Dhamapāla The Revialist of Buddhism</span>
 								</div>
 							</a>
 						</div>
 						<div class="col-md-3">
-							<a href="#" class="photography-entry img d-flex justify-content-center align-items-center" style="background-image: url(images/image_9.jpg);">
+							<a href="#" class="photography-entry img d-flex justify-content-center align-items-center" style="background-image: url(cover/9.Gandhavaṃsa.jpg);">
 								<div class="overlay"></div>
 								<div class="text text-center">
-									<h3>Work 09</h3>
-									<span>Technology</span>
+									<span>Gandhavaṃsa</span>
 								</div>
 							</a>
 						</div>
 						<div class="col-md-3">
-							<a href="#" class="photography-entry img d-flex justify-content-center align-items-center" style="background-image: url(images/image_10.jpg);">
+							<a href="#" class="photography-entry img d-flex justify-content-center align-items-center" style="background-image: url(cover/10.Bauddha_Dhama_ke_Punaruddharaka_Tripitakacharya_Dr.Bhikshu_Dharmarakshit.jpg);">
 								<div class="overlay"></div>
 								<div class="text text-center">
-									<h3>Work 10</h3>
-									<span>Model</span>
+									<span>Bauddha Dhama ke Punaruddharaka Tripitakacharya Dr.Bhikshu Dharmarakshit</span>
 								</div>
 							</a>
 						</div>
 						<div class="col-md-3">
-							<a href="#" class="photography-entry img d-flex justify-content-center align-items-center" style="background-image: url(images/image_11.jpg);">
+							<a href="#" class="photography-entry img d-flex justify-content-center align-items-center" style="background-image: url(cover/11.The_Revivalist_of_Buddhism_Anagārika_Dharmapāla_and_Saṅgharāja_Saraṇaṅkara.jpg);">
 								<div class="overlay"></div>
 								<div class="text text-center">
-									<h3>Work 11</h3>
-									<span>Fashion</span>
+									<span>The Revivalist of Buddhism Anagārika Dharmapāla and Saṅgharāja Saraṇaṅkara</span>
 								</div>
 							</a>
 						</div>
 						<div class="col-md-3">
-							<a href="#" class="photography-entry img d-flex justify-content-center align-items-center" style="background-image: url(images/image_12.jpg);">
+							<a href="#" class="photography-entry img d-flex justify-content-center align-items-center" style="background-image: url(cover/12.Human_Values_and_Buddhist_Ethics.jpg);">
 								<div class="overlay"></div>
 								<div class="text text-center">
-									<h3>Work 12</h3>
-									<span>Photography</span>
+									<span>Human Values and Buddhist Ethics</span>
 								</div>
 							</a>
 						</div>
